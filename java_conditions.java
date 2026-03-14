98f0ce7db9c83857896f34df25bbf62d59931c38
D. B. Sehansa/Python🐍 <sehansadewlini27@gmail.com>
2026-03-14 22:46:03 +0530

    Implement minimum number finder using command line args
    Added command line argument handling to find the minimum of two numbers.

--- java_conditions.java
+++ java_conditions.java
@@ -1,4 +1,5 @@
 // Higher diploma in Information Technology - Year one Java programming
+// problem:
 // Write a program to get 2 values as command line arguments and find the Minimum of those 2 using if-else.
 
 class java_conditions{
@@ -8,11 +9,13 @@ class java_conditions{
             return;
         }
 
+        // get user input with command line arguments
         int num1=Integer.parseInt(args[0]);
         int num2=Integer.parseInt(args[1]);
 
         int minimumNumber;
 
+        // find the maximum number using conditional statements
         if(num1<num2){
             minimumNumber=num1;
         }
@@ -20,7 +23,11 @@ class java_conditions{
             minimumNumber=num2;
         }
 
+        // print the output
         System.out.println("The minimum number is: "+minimumNumber);
 
     }
 }
+
+// sample output:
+// The minimum number is: 10
